eff53745d91ded60a2cefbe2b859ecbba96dc03f
Dustin Carlino <dabreegster@gmail.com>
2024-02-26 14:56:12 +0000

    Reorder lanes (without bringing in a drag-and-drop library yet)

--- src/routes/cross_section/proposed/CrossSection.svelte
+++ src/routes/cross_section/proposed/CrossSection.svelte
@@ -12,12 +12,33 @@
     lanesLeftToRight.splice(i, 1);
     lanesLeftToRight = lanesLeftToRight;
   }
+
+  function moveLeft(i: number) {
+    [lanesLeftToRight[i - 1], lanesLeftToRight[i]] = [
+      lanesLeftToRight[i],
+      lanesLeftToRight[i - 1],
+    ];
+  }
+
+  function moveRight(i: number) {
+    [lanesLeftToRight[i + 1], lanesLeftToRight[i]] = [
+      lanesLeftToRight[i],
+      lanesLeftToRight[i + 1],
+    ];
+  }
 </script>
 
 <SecondaryButton on:click={addNewLane}>Add</SecondaryButton>
 
 <div style="display: flex; flex-direction: row">
   {#each lanesLeftToRight as lane, i (i)}
-    <LaneCard bind:value={lane} on:delete={() => deleteLane(i)} />
+    <LaneCard
+      bind:value={lane}
+      on:delete={() => deleteLane(i)}
+      on:moveLeft={() => moveLeft(i)}
+      on:moveRight={() => moveRight(i)}
+      isFirst={i == 0}
+      isLast={i == lanesLeftToRight.length - 1}
+    />
   {/each}
 </div>
--- src/routes/cross_section/proposed/LaneCard.svelte
+++ src/routes/cross_section/proposed/LaneCard.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
   import { createEventDispatcher } from "svelte";
-  import { WarningButton, Select } from "govuk-svelte";
+  import { SecondaryButton, WarningButton, Select } from "govuk-svelte";
   import { laneTypes } from "../data";
 
   export let value: string;
+  export let isFirst: boolean;
+  export let isLast: boolean;
 
   let dispatch = createEventDispatcher<{
     delete: void;
+    moveLeft: void;
+    moveRight: void;
   }>();
 
   function pairs(list: string[]): [string, string][] {
@@ -16,7 +20,15 @@
 
 <div>
   <Select label="Lane type" emptyOption choices={pairs(laneTypes)} bind:value />
-  <WarningButton on:click={() => dispatch("delete")}>Delete</WarningButton>
+  <div style="display: flex; justify-content: space-between">
+    <SecondaryButton disabled={isFirst} on:click={() => dispatch("moveLeft")}>
+      &larr;
+    </SecondaryButton>
+    <WarningButton on:click={() => dispatch("delete")}>Delete</WarningButton>
+    <SecondaryButton disabled={isLast} on:click={() => dispatch("moveRight")}>
+      &rarr;
+    </SecondaryButton>
+  </div>
 </div>
 
 <style>
