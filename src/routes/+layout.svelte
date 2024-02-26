a39b9f906be42ddf4a0c9ebd9b6101173dce3272
Dustin Carlino <dabreegster@gmail.com>
2024-02-26 10:17:12 +0000

    Sveltekit starter, configured without a server

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<nav>
+  <a href="/">home</a>
+</nav>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let counter = 0;
+</script>
+
+<p>Counter is {counter}</p>
+<button
+  type="button"
+  on:click={() => {
+    counter++;
+  }}
+>
+  Increment
+</button>
