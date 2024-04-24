546f2d3d65f75ade5f66c3109e4a18093b93d05e
Dustin Carlino <dabreegster@gmail.com>
2024-04-24 09:58:32 +0100

    Manually fix rebase

---
+++ src/routes/route_check/results_summary/LevelOfServiceResults.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let title: string;
+  export let existing: number;
+  export let proposed: number;
+  let netDifference = proposed - existing;
+</script>
+
+<div class="header">
+  {title}
+</div>
+<div class="grid-box">{existing}%</div>
+<div class="grid-box">{proposed}%</div>
+<div class="grid-box">{netDifference}%</div>
+
+<style>
+  .header {
+    background-color: #006853;
+    color: white;
+    border: 1px solid white;
+    text-align: center;
+  }
+  .grid-box {
+    text-align: center;
+    border: 1px solid black;
+  }
+</style>
