1f490f2c25f5979da7543051a6d83148e3013003
Dustin Carlino <dabreegster@gmail.com>
2024-04-24 15:38:46 +0100

    Focus on the marker when editing its form. #22

--- src/routes/route_check/critical_issues_log/MapEntry.svelte
+++ src/routes/route_check/critical_issues_log/MapEntry.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
+  import type { Feature, Position } from "geojson";
   import Form from "./Form.svelte";
   import { DefaultButton, WarningButton } from "govuk-svelte";
   import { onMount, onDestroy } from "svelte";
   import { DraggablePin } from "$lib/map";
   import { MapLibreMap } from "$lib/map";
+  import { GeoJSON, CircleLayer } from "svelte-maplibre";
   import type { MapMouseEvent, LngLat, Map } from "maplibre-gl";
   import { state } from "../data";
 
   let map: Map;
 
   let editing: number | null = null;
-  // TODO while editing, emphasize that pin and disable all map interactions
 
   function onMarkerClick(idx: number) {
     editing = idx;
@@ -45,6 +46,17 @@
     $state.criticalIssues = $state.criticalIssues;
     editing = null;
   }
+
+  function pointFeature(coordinates: Position): Feature {
+    return {
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Point",
+        coordinates,
+      },
+    };
+  }
 </script>
 
 <div style="display: flex">
@@ -53,12 +65,15 @@
       <p>Click the map to add a pin, or click a pin to fill out data</p>
     {:else}
       <p>Editing {editing}</p>
-      <Form idx={editing} />
       <DefaultButton on:click={() => (editing = null)}>Save</DefaultButton>
       <WarningButton on:click={deleteCritical}>Delete</WarningButton>
+      <Form idx={editing} />
     {/if}
   </div>
   <div style="position: relative; width: 70%; height: 300px;">
+    {#if editing != null}
+      <div class="block-map" />
+    {/if}
     <MapLibreMap bind:map>
       {#each $state.criticalIssues as issue, idx}
         <DraggablePin
@@ -67,6 +82,26 @@
           on:click={() => onMarkerClick(idx)}
         />
       {/each}
+
+      {#if editing != null}
+        <GeoJSON data={pointFeature($state.criticalIssues[editing].point)}>
+          <CircleLayer
+            paint={{ "circle-radius": 10, "circle-color": "yellow" }}
+          />
+        </GeoJSON>
+      {/if}
     </MapLibreMap>
   </div>
 </div>
+
+<style>
+  .block-map {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.1);
+    z-index: 999;
+  }
+</style>
