eb625d9b318cf63d9da3fc17d5ecea0f77737042
Dustin Carlino <dabreegster@gmail.com>
2024-04-29 12:35:37 +0100

    Trying to rebuild the FancyRadio component to use govuk styles/components consistently
    Attempt 1: one row, three independent columns can't keep description
    lined up with scores

--- src/lib/FancyRadio.svelte
+++ src/lib/FancyRadio.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { TextArea } from "govuk-svelte";
+
   import { v4 as uuidv4 } from "uuid";
 
   // A list of [value, label, background color, font color] representing the choices
@@ -13,139 +15,94 @@
   let id = uuidv4();
 </script>
 
-<div class="govuk-radios fancy-radio-grid" data-module="govuk-radios">
-  <div
-    class="govuk-font"
-    style="grid-column:1/2; grid-row:1/2; font-weight: bold"
-  >
-    Score for existing infrastructure:
-  </div>
-  <div
-    class="govuk-font"
-    style="grid-column:2/3; grid-row:1/2; font-weight: bold"
-  >
-    Score for proposed infrastructure:
-  </div>
-
-  {#each choices as [thisValue, thisLabel, thisBackgroundColor, thisFontColour], i}
-    <div
-      class="govuk-radios__item"
-      style="grid-row:{i + 2}/{i + 3}; grid-column:1/2"
-    >
-      <input
-        class="govuk-radios__input"
-        id={`${id}${thisValue}-existing`}
-        type="radio"
-        value={thisValue}
-        bind:group={existingValue}
-      />
-
-      <label
-        class="govuk-label govuk-radios__label"
-        for={`${id}${thisValue}-existing`}
-      >
-        <span
-          class="option-value"
-          style:background={thisBackgroundColor}
-          style:color={thisFontColour}
-        >
-          {thisValue}
-        </span>
-      </label>
+<div class="govuk-grid-row">
+  <!-- First column -->
+  <div class="govuk-grid-column-one-third" style="border: 1px solid black">
+    <div class="govuk-form-group">
+      <fieldset class="govuk-fieldset">
+        <legend class="govuk-fieldset__legend govuk-label--s">
+          Score for existing infrastructure
+        </legend>
+        <div class="govuk-radios" data-module="govuk-radios">
+          {#each choices as [thisValue, thisLabel, thisBackgroundColor, thisFontColour], i}
+            <div class="govuk-radios__item">
+              <input
+                class="govuk-radios__input"
+                id={id + thisValue + "-existing"}
+                type="radio"
+                value={thisValue}
+                bind:group={existingValue}
+              />
+              <label
+                class="govuk-label govuk-radios__label"
+                for={id + thisValue + "-existing"}
+              >
+                <span
+                  style:background={thisBackgroundColor}
+                  style:color={thisFontColour}
+                >
+                  {thisValue}
+                </span>
+              </label>
+            </div>
+            <hr />
+          {/each}
+        </div>
+      </fieldset>
     </div>
 
-    <div
-      class="govuk-radios__item"
-      style="grid-row:{i + 2}/{i + 3}; grid-column:2/3"
-    >
-      <input
-        class="govuk-radios__input"
-        id={`${id}${thisValue}-proposed`}
-        type="radio"
-        value={thisValue}
-        bind:group={proposedValue}
-      />
+    <TextArea
+      label="Notes to justify given score for existing infrastructure"
+      bind:value={existingNotes}
+    />
+  </div>
 
-      <label
-        class="govuk-label govuk-radios__label"
-        for={`${id}${thisValue}-proposed`}
-      >
-        <span
-          class="option-value"
-          style:background={thisBackgroundColor}
-          style:color={thisFontColour}
-        >
-          {thisValue}
-        </span>
-      </label>
+  <!-- Second column -->
+  <div class="govuk-grid-column-one-third" style="border: 1px solid black">
+    <div class="govuk-form-group">
+      <fieldset class="govuk-fieldset">
+        <legend class="govuk-fieldset__legend govuk-label--s">
+          Score for proposed infrastructure
+        </legend>
+        <div class="govuk-radios" data-module="govuk-radios">
+          {#each choices as [thisValue, thisLabel, thisBackgroundColor, thisFontColour], i}
+            <div class="govuk-radios__item">
+              <input
+                class="govuk-radios__input"
+                id={id + thisValue + "-proposed"}
+                type="radio"
+                value={thisValue}
+                bind:group={proposedValue}
+              />
+              <label
+                class="govuk-label govuk-radios__label"
+                for={id + thisValue + "-proposed"}
+              >
+                <span
+                  style:background={thisBackgroundColor}
+                  style:color={thisFontColour}
+                >
+                  {thisValue}
+                </span>
+              </label>
+            </div>
+            <hr />
+          {/each}
+        </div>
+      </fieldset>
     </div>
 
-    <div
-      class="option-text govuk-font"
-      style="grid-row:{i + 2}/{i + 3}; grid-column:3/4"
-    >
-      <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    <TextArea
+      label="Notes to justify given score for proposed infrastructure"
+      bind:value={proposedNotes}
+    />
+  </div>
+
+  <!-- Third column -->
+  <div class="govuk-grid-column-one-third" style="border: 1px solid black">
+    {#each choices as [thisValue, thisLabel, thisBackgroundColor, thisFontColour], i}
       {@html thisLabel}
-    </div>
-  {/each}
-
-  <label
-    for="existing-comments-box"
-    class="govuk-font"
-    style="grid-row:{choices.length + 2}/{choices.length + 3}; grid-column:1/2"
-  >
-    Notes to justify given score for existing infrastructure:
-  </label>
-  <label
-    for="proposed-comments-box"
-    class="govuk-font"
-    style="grid-row:{choices.length + 2}/{choices.length + 3}; grid-column:2/3"
-  >
-    Notes to justify given score for proposed infrastructure:
-  </label>
-
-  <div />
-  <textarea
-    id="existing-comments-box"
-    class="govuk-textarea comments-box"
-    style="grid-row:{choices.length + 3}/{choices.length + 4}; grid-column:1/2"
-    bind:value={existingNotes}
-  />
-  <textarea
-    id="proposed-comments-box"
-    class="govuk-textarea comments-box"
-    style="grid-row:{choices.length + 3}/{choices.length + 4}; grid-column:2/3"
-    bind:value={proposedNotes}
-  />
+      <hr />
+    {/each}
+  </div>
 </div>
-
-<style>
-  span.option-value {
-    padding: 8px;
-    height: 24px;
-    border-radius: 20px;
-    margin-right: 5px;
-  }
-
-  .govuk-radios__item {
-    min-width: 106px;
-    margin-bottom: 10px;
-    align-content: flex-start;
-  }
-
-  .fancy-radio-grid {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr) 2fr;
-    grid-column-gap: 1rem;
-    grid-row-gap: 1rem;
-    max-height: 80vh;
-  }
-
-  .govuk-font {
-    font-family: "GDS Transport", arial, sans-serif;
-  }
-
-  .comments-box {
-    height: 100%;
-  }
-</style>
