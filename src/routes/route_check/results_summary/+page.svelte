43f465d0a65e3329e5c7d1f7960bbaec25ce0f14
Dustin Carlino <dabreegster@gmail.com>
2024-05-01 16:27:49 +0100

    Start redoing the results page

--- src/routes/route_check/results_summary/+page.svelte
+++ src/routes/route_check/results_summary/+page.svelte
@@ -2,31 +2,23 @@
   import { TextArea } from "govuk-svelte";
   import { sum } from "$lib";
   import { state, type Scorecard, numericScore } from "../data";
-  import NetDifferenceResults from "./NetDifferenceResults.svelte";
-  import LevelOfServiceResults from "./LevelOfServiceResults.svelte";
 
-  const policyCheckComplete =
-    $state.policyCheck.filter((policyCheckObject) => {
-      return (
-        policyCheckObject.existing === "" || policyCheckObject.proposed === ""
-      );
-    }).length === 0;
-  const policyIssuesForReview = $state.policyConflictLog.length;
-
-  const safetyCheckComplete = isScorecardCompleted($state.safetyCheck);
-  const safetyIssuesForReview = $state.criticalIssues.length;
-
-  const streetCheckComplete = isScorecardCompleted($state.streetCheck);
-
-  const streetPlacemakingCheckComplete = isScorecardCompleted(
-    $state.streetPlacemakingCheck,
+  let policyCheckComplete = $state.policyCheck.every(
+    (x) => x.existing != "" && x.proposed != "",
   );
+  let incompletePolicyConflicts = $state.policyConflictLog.filter(
+    (x) => x.resolved != "Yes",
+  ).length;
 
-  const pathCheckComplete = isScorecardCompleted($state.pathCheck);
+  // TODO Also looks at critical log
+  let safetyCheckComplete = isScorecardCompleted($state.safetyCheck);
+  let incompleteCriticalIssues = $state.criticalIssues.filter(
+    (x) => x.resolved != "Yes",
+  ).length;
 
-  const pathPlacemakingCheckComplete = isScorecardCompleted(
-    $state.pathPlacemakingCheck,
-  );
+  function yesNo(x: boolean): string {
+    return x ? "Yes" : "No";
+  }
 
   function isScorecardCompleted(scorecard: Scorecard): boolean {
     return (
@@ -35,132 +27,138 @@
     );
   }
 
-  function getScorecardDifference(scorecard: Scorecard): number {
+  /*function getScorecardDifference(scorecard: Scorecard): number {
     let existing = sum(scorecard.existingScores.map(numericScore));
     let proposed = sum(scorecard.proposedScores.map(numericScore));
     return proposed - existing;
-  }
+  }*/
 </script>
 
-<div class="results-grid">
-  <!-- TODO No rows in the markup? -->
-  <div class="header">Overview</div>
-  <div class="header">Complete</div>
-  <div class="header">Remaining Isuses for Review</div>
-  <div class="header">Next Steps</div>
+<h2>Overview</h2>
 
-  <div class="header">Policy Conflicts</div>
-  <div class="grid-box">{policyCheckComplete}</div>
-  <div class="grid-box">{policyIssuesForReview}</div>
-  <div class="grid-box">
-    {policyIssuesForReview > 0 || !policyCheckComplete
-      ? "View Comments in Policy Check and Policy Conflict Log tabs"
-      : "No further Action"}
-  </div>
+<table>
+  <tr>
+    <th></th>
+    <th>Complete</th>
+    <th>Remaining issues for review</th>
+    <th>Next steps</th>
+  </tr>
 
-  <div class="header">Critical Issues</div>
-  <div class="grid-box">{safetyCheckComplete}</div>
-  <div class="grid-box">{safetyIssuesForReview}</div>
-  <div class="grid-box">
-    {safetyIssuesForReview > 0 || !safetyCheckComplete
-      ? "Complete Safety Check and Critical Issues Log"
-      : "No further Action"}
-  </div>
+  <tr>
+    <th>Policy Conflicts</th>
+    <td>{yesNo(policyCheckComplete)}</td>
+    <td>{incompletePolicyConflicts}</td>
+    <td>
+      {#if !policyCheckComplete}
+        Complete Policy Check and Policy Conflict Log
+      {:else if incompletePolicyConflicts > 0}
+        View Comments in Policy Check and Policy Conflict Log tabs
+      {:else}
+        No further action
+      {/if}
+    </td>
+  </tr>
 
-  <div class="header" />
-  <div class="header" style="grid-column: 2/5;">Net Difference</div>
-  <NetDifferenceResults
-    title="Safety Check"
-    isComplete={safetyCheckComplete}
-    netDifference={getScorecardDifference($state.safetyCheck)}
-    summaryNoun="safety"
-  />
-  <NetDifferenceResults
-    title="Street Check"
-    isComplete={streetCheckComplete}
-    netDifference={getScorecardDifference($state.streetCheck)}
-    summaryNoun="the route quality"
-  />
-  <NetDifferenceResults
-    title="Street Placemaking"
-    isComplete={streetPlacemakingCheckComplete}
-    netDifference={getScorecardDifference($state.streetPlacemakingCheck)}
-    summaryNoun="the quality of place"
-  />
-  <NetDifferenceResults
-    title="Path Check"
-    isComplete={pathCheckComplete}
-    netDifference={getScorecardDifference($state.pathCheck)}
-    summaryNoun="the route quality"
-  />
-  <NetDifferenceResults
-    title="Path Placemaking"
-    isComplete={pathPlacemakingCheckComplete}
-    netDifference={getScorecardDifference($state.pathPlacemakingCheck)}
-    summaryNoun="the quality of place"
-  />
-  <NetDifferenceResults
-    title="Junction Assessment Tool"
-    isComplete={false}
-    netDifference={-105}
-    summaryNoun="TODO"
-  />
-</div>
+  <tr>
+    <th>Critical Issues</th>
+    <td>{yesNo(safetyCheckComplete)}</td>
+    <td>{incompleteCriticalIssues}</td>
+    <td>
+      {#if !safetyCheckComplete}
+        Complete Safety Check and Critical Issues Log
+      {:else if incompleteCriticalIssues > 0}
+        View Comments in Critical Issues Log
+      {:else}
+        No further action
+      {/if}
+    </td>
+  </tr>
 
-<div class="level-of-service-results">
-  <div class="header" style="grid-column: 1/5; grid-row:1/2">
-    Street Check Level of Service
-  </div>
-  <div class="header" style="grid-column: 1/2; grid-row:2/3">Categories</div>
-  <div class="header" style="grid-column: 2/3; grid-row:2/3">
-    Existing Layout
-  </div>
-  <div class="header" style="grid-column: 3/4; grid-row:2/3">
-    Proposed Layout
-  </div>
-  <div class="header" style="grid-column: 4/5; grid-row:2/3">
-    Net Difference
-  </div>
+  <tr>
+    <th></th>
+    <th>Complete</th>
+    <th>Net difference</th>
+    <th>Next steps</th>
+  </tr>
 
-  <LevelOfServiceResults title="Safety" existing={2} proposed={68} />
-  <LevelOfServiceResults title="Accessibility" existing={2} proposed={68} />
-  <LevelOfServiceResults title="Comfort" existing={2} proposed={68} />
-  <LevelOfServiceResults title="Directness" existing={2} proposed={68} />
-  <LevelOfServiceResults title="Attractiveness" existing={2} proposed={68} />
-  <LevelOfServiceResults title="Cohesion" existing={2} proposed={68} />
-  <div class="header" style="grid-column: 1/5" />
-  <LevelOfServiceResults title="Overall ATE Score" existing={2} proposed={68} />
-</div>
+  <tr>
+    <th>Safety Check</th>
+    <td>{yesNo(safetyCheckComplete)}</td>
+    <td>TODO</td>
+    <td>TODO</td>
+  </tr>
+
+  <tr>
+    <th>Street Check</th>
+    {#if $state.summary.checkType == "street"}
+      <td>{yesNo(isScorecardCompleted($state.streetCheck))}</td>
+      <td>TODO</td>
+      <td>TODO</td>
+    {:else}
+      <td>N/A</td>
+      <td>N/A</td>
+      <td>N/A</td>
+    {/if}
+  </tr>
+
+  <tr>
+    <th>Street Placemaking</th>
+    {#if $state.summary.checkType == "street"}
+      <td>{yesNo(isScorecardCompleted($state.streetPlacemakingCheck))}</td>
+      <td>TODO</td>
+      <td>TODO</td>
+    {:else}
+      <td>N/A</td>
+      <td>N/A</td>
+      <td>N/A</td>
+    {/if}
+  </tr>
+
+  <tr>
+    <th>Path Check</th>
+    {#if $state.summary.checkType == "path"}
+      <td>{yesNo(isScorecardCompleted($state.pathCheck))}</td>
+      <td>TODO</td>
+      <td>TODO</td>
+    {:else}
+      <td>N/A</td>
+      <td>N/A</td>
+      <td>N/A</td>
+    {/if}
+  </tr>
+
+  <tr>
+    <th>Path Placemaking</th>
+    {#if $state.summary.checkType == "path"}
+      <td>{yesNo(isScorecardCompleted($state.pathPlacemakingCheck))}</td>
+      <td>TODO</td>
+      <td>TODO</td>
+    {:else}
+      <td>N/A</td>
+      <td>N/A</td>
+      <td>N/A</td>
+    {/if}
+  </tr>
+
+  <tr>
+    <th>Junction Assessment Tool Check</th>
+    <td>TODO</td>
+    <td>TODO</td>
+    <td>TODO</td>
+  </tr>
+</table>
 
 <!-- TODO Hint: "Use the space to provide overall feedback for the proposed scheme" -->
 <TextArea label="Review statement" bind:value={$state.resultsReviewStatement} />
 
 <style>
-  .results-grid {
-    display: grid;
-
-    grid-template-columns: repeat(4, 1fr);
-    grid-template-rows: repeat(3, 2fr) 1fr repeat(6, 2fr);
-  }
-  .header {
-    /* TODO Plumb from colors.green.background */
-    background-color: #006853;
-    /* TODO Plumb from colors.green.font */
-    color: white;
-    border: 1px solid white;
-    text-align: center;
-  }
-  .grid-box {
-    text-align: center;
+  td {
     border: 1px solid black;
+    padding: 4px;
   }
 
-  .level-of-service-results {
-    display: grid;
-    grid-template-columns: 2fr repeat(3, 1fr);
-    grid-template-rows: repeat(2, 2f3) repeat(8, 1fr);
-  }
-  div {
-    display: inline;
+  th {
+    background: #006853;
+    color: white;
   }
 </style>
