a6fa0d80f68be3aa10123466a61ee38157b1dfbe
Dustin Carlino <dabreegster@gmail.com>
2024-05-02 10:56:03 +0100

    Continue with results page. Some calculations look wrong.

--- src/routes/route_check/results_summary/+page.svelte
+++ src/routes/route_check/results_summary/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import { base } from "$app/paths";
   import { TextArea } from "govuk-svelte";
   import { sum } from "$lib";
   import { state, type Scorecard, numericScore } from "../data";
+  import LevelOfServiceRow from "./LevelOfServiceRow.svelte";
 
   let policyCheckComplete = $state.policyCheck.every(
     (x) => x.existing != "" && x.proposed != "",
@@ -27,11 +29,19 @@
     );
   }
 
-  /*function getScorecardDifference(scorecard: Scorecard): number {
-    let existing = sum(scorecard.existingScores.map(numericScore));
-    let proposed = sum(scorecard.proposedScores.map(numericScore));
-    return proposed - existing;
-  }*/
+  // TODO Move to a .ts file, calculate a summaty, then make some views on it
+  // Returns [existing, proposed] total score as a percentage
+  function scoreDifference(scorecard: Scorecard): [number, number] {
+    let numberMetrics = scorecard.existingScores.filter(
+      (x) => x != "N/A",
+    ).length;
+    // For safety, the scores are multiplied by 3 (or 6
+    let maxScore = numberMetrics * 6;
+
+    let existingScore = 3 * sum(scorecard.existingScores.map(numericScore));
+    let proposedScore = 3 * sum(scorecard.proposedScores.map(numericScore));
+    return [(existingScore / maxScore) * 100, (proposedScore / maxScore) * 100];
+  }
 </script>
 
 <h2>Overview</h2>
@@ -45,7 +55,9 @@
   </tr>
 
   <tr>
-    <th>Policy Conflicts</th>
+    <th>
+      <a href="{base}/route_check/critical_issues_log">Policy Conflicts</a>
+    </th>
     <td>{yesNo(policyCheckComplete)}</td>
     <td>{incompletePolicyConflicts}</td>
     <td>
@@ -60,7 +72,9 @@
   </tr>
 
   <tr>
-    <th>Critical Issues</th>
+    <th>
+      <a href="{base}/route_check/critical_issues_log">Critical Issues</a>
+    </th>
     <td>{yesNo(safetyCheckComplete)}</td>
     <td>{incompleteCriticalIssues}</td>
     <td>
@@ -82,19 +96,25 @@
   </tr>
 
   <tr>
-    <th>Safety Check</th>
+    <th><a href="{base}/route_check/safety_check">Safety Check</a></th>
     <td>{yesNo(safetyCheckComplete)}</td>
-    <td>TODO</td>
+    <td>
+      {Math.round(
+        scoreDifference($state.safetyCheck)[1] -
+          scoreDifference($state.safetyCheck)[0],
+      )}%
+    </td>
     <td>TODO</td>
   </tr>
 
   <tr>
-    <th>Street Check</th>
     {#if $state.summary.checkType == "street"}
+      <th><a href="{base}/route_check/street_check">Street Check</a></th>
       <td>{yesNo(isScorecardCompleted($state.streetCheck))}</td>
       <td>TODO</td>
       <td>TODO</td>
     {:else}
+      <th>Street Check</th>
       <td>N/A</td>
       <td>N/A</td>
       <td>N/A</td>
@@ -102,12 +122,17 @@
   </tr>
 
   <tr>
-    <th>Street Placemaking</th>
     {#if $state.summary.checkType == "street"}
+      <th>
+        <a href="{base}/route_check/street_placemaking_check">
+          Street Placemaking
+        </a>
+      </th>
       <td>{yesNo(isScorecardCompleted($state.streetPlacemakingCheck))}</td>
       <td>TODO</td>
       <td>TODO</td>
     {:else}
+      <th>Street Placemaking</th>
       <td>N/A</td>
       <td>N/A</td>
       <td>N/A</td>
@@ -115,12 +140,13 @@
   </tr>
 
   <tr>
-    <th>Path Check</th>
     {#if $state.summary.checkType == "path"}
+      <th><a href="{base}/route_check/path_check">Path Check</a></th>
       <td>{yesNo(isScorecardCompleted($state.pathCheck))}</td>
       <td>TODO</td>
       <td>TODO</td>
     {:else}
+      <th>Path Check</th>
       <td>N/A</td>
       <td>N/A</td>
       <td>N/A</td>
@@ -128,12 +154,15 @@
   </tr>
 
   <tr>
-    <th>Path Placemaking</th>
     {#if $state.summary.checkType == "path"}
+      <th>
+        <a href="{base}/route_check/path_placemaking_check">Path Placemaking</a>
+      </th>
       <td>{yesNo(isScorecardCompleted($state.pathPlacemakingCheck))}</td>
       <td>TODO</td>
       <td>TODO</td>
     {:else}
+      <th>Path Placemaking</th>
       <td>N/A</td>
       <td>N/A</td>
       <td>N/A</td>
@@ -141,11 +170,51 @@
   </tr>
 
   <tr>
-    <th>Junction Assessment Tool Check</th>
+    <th>
+      <a href="{base}/route_check/jat_check">Junction Assessment Tool Check</a>
+    </th>
     <td>TODO</td>
     <td>TODO</td>
     <td>TODO</td>
   </tr>
+</table>
+
+<h2>Street Check Level of Service</h2>
+
+<table>
+  <tr>
+    <th>Categories</th>
+    <th>Existing Layout</th>
+    <th>Proposed Layout</th>
+    <th>Net difference</th>
+  </tr>
+
+  <LevelOfServiceRow category="Safety" scorecard={$state.safetyCheck} />
+  <LevelOfServiceRow
+    category="Accessibility"
+    scorecard={$state.streetCheck}
+    range={[0, 6]}
+  />
+  <LevelOfServiceRow
+    category="Comfort"
+    scorecard={$state.streetCheck}
+    range={[7, 9]}
+  />
+  <LevelOfServiceRow
+    category="Directness"
+    scorecard={$state.streetCheck}
+    range={[10, 15]}
+  />
+  <LevelOfServiceRow
+    category="Attractiveness"
+    scorecard={$state.streetCheck}
+    range={[16, 21]}
+  />
+  <LevelOfServiceRow
+    category="Cohesion"
+    scorecard={$state.streetCheck}
+    range={[22, 25]}
+  />
 </table>
 
 <!-- TODO Hint: "Use the space to provide overall feedback for the proposed scheme" -->
---
+++ src/routes/route_check/results_summary/LevelOfServiceRow.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import { sum } from "$lib";
+  import { type Scorecard, numericScore } from "../data";
+
+  export let category: string;
+  export let scorecard: Scorecard;
+  // Inclusive
+  export let range: [number, number] | null = null;
+
+  let [existing, proposed] = scoreDifference();
+
+  // Returns [existing, proposed] total score as a percentage
+  function scoreDifference(): [number, number] {
+    let actualRange = range || [0, scorecard.existingScores.length - 1];
+    let idx1 = actualRange[0];
+    let idx2 = actualRange[1] + 1;
+
+    // TODO Same denominator for existing/proposed, assuming the N/A's match
+    let numberMetrics = scorecard.existingScores
+      .slice(idx1, idx2)
+      .filter((x) => x != "N/A").length;
+
+    let multiplier = category == "Safety" ? 3 : 1;
+    let maxScore = numberMetrics * 2 * multiplier;
+
+    let existingScore =
+      multiplier *
+      sum(scorecard.existingScores.slice(idx1, idx2).map(numericScore));
+    let proposedScore =
+      multiplier *
+      sum(scorecard.proposedScores.slice(idx1, idx2).map(numericScore));
+    return [(existingScore / maxScore) * 100, (proposedScore / maxScore) * 100];
+  }
+</script>
+
+<tr>
+  <td>{category}</td>
+  <td>{Math.round(existing)}%</td>
+  <td>{Math.round(proposed)}%</td>
+  <td>{Math.round(proposed - existing)}%</td>
+</tr>
+
+<style>
+  td {
+    border: 1px solid black;
+    padding: 4px;
+  }
+
+  th {
+    background: #006853;
+    color: white;
+  }
+</style>
