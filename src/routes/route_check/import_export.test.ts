984500d79afc41d56702945859b233c887089dab
Dustin Carlino <dabreegster@gmail.com>
2024-05-15 16:33:03 +0100

    Make more results tests, covering street checks. Already (nearly) passing! #36

--- src/routes/route_check/import_export.test.ts
+++ src/routes/route_check/import_export.test.ts
@@ -62,6 +62,99 @@ describe("check path results", async () => {
   });
 });
 
+describe("check street results, example 1", async () => {
+  let workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile("test_files/street_1.xlsx");
+  let inputDalog = getDalog(workbook);
+  let state = dalogToState(inputDalog);
+  let results = getResults(state);
+
+  it("level of service", () => {
+    expect([...results.levelOfService, results.overall].map(row)).toStrictEqual(
+      [
+        ["Safety", 27, 35, "8%"],
+        ["Accessibility", 25, 25, "0%"],
+        ["Comfort", 67, 67, "0%"],
+        ["Directness", 75, 75, "0%"],
+        ["Attractiveness", 58, 58, "0%"],
+        ["Cohesion", 50, 50, "0%"],
+        ["overall", 36, 41, "5%"],
+      ],
+    );
+  });
+
+  it("by mode", () => {
+    expect([...results.byMode, results.overall].map(row)).toStrictEqual([
+      ["Walking", 34, 34, "0%"],
+      ["Wheeling", 35, 35, "0%"],
+      ["Cycling", 37, 45, "8%"],
+      ["overall", 36, 41, "5%"],
+    ]);
+  });
+
+  it("placemaking", () => {
+    expect(
+      [...results.placemakingCategories, results.placemakingOverall].map(row),
+    ).toStrictEqual([
+      ["Social activity", 0, 0, "0%"],
+      ["Personal security", 67, 67, "0%"],
+      ["Character and legibility", 45, 45, "0%"],
+      ["Environment", 40, 40, "0%"],
+      ["overall", 40, 40, "0%"],
+    ]);
+  });
+});
+
+describe("check street results, example 2", async () => {
+  let workbook = new ExcelJS.Workbook();
+  await workbook.xlsx.readFile("test_files/street_2.xlsx");
+  let inputDalog = getDalog(workbook);
+  let state = dalogToState(inputDalog);
+  let results = getResults(state);
+
+  console.log([...results.levelOfService, results.overall].map(row));
+  console.log([...results.byMode, results.overall].map(row));
+  console.log(
+    [...results.placemakingCategories, results.placemakingOverall].map(row),
+  );
+
+  it("level of service", () => {
+    expect([...results.levelOfService, results.overall].map(row)).toStrictEqual(
+      [
+        ["Safety", 14, 21, "7%"],
+        ["Accessibility", 75, 50, "-25%"],
+        ["Comfort", 100, 67, "-33%"],
+        ["Directness", 50, 60, "10%"],
+        ["Attractiveness", 17, 17, "0%"],
+        // TODO different rounding
+        ["Cohesion", 38, 25, "-13%"],
+        ["overall", 25, 28, "3%"],
+      ],
+    );
+  });
+
+  it("by mode", () => {
+    expect([...results.byMode, results.overall].map(row)).toStrictEqual([
+      ["Walking", 22, 25, "3%"],
+      ["Wheeling", 22, 24, "2%"],
+      ["Cycling", 23, 29, "6%"],
+      ["overall", 25, 28, "3%"],
+    ]);
+  });
+
+  it("placemaking", () => {
+    expect(
+      [...results.placemakingCategories, results.placemakingOverall].map(row),
+    ).toStrictEqual([
+      ["Social activity", 0, 0, "0%"],
+      ["Personal security", 50, 50, "0%"],
+      ["Character and legibility", 50, 50, "0%"],
+      ["Environment", 40, 45, "5%"],
+      ["overall", 40, 42, "2%"],
+    ]);
+  });
+});
+
 function row(x: ResultCategory): [string, number, number, string] {
   return [
     x.category,
