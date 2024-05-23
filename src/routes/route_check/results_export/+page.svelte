81812eaa460acc1aa7ed7b0d5bc3b1f0399428ae
Dustin Carlino <dabreegster@gmail.com>
2024-05-23 11:07:46 +0100

    Add result export tables for remaining criticals/conflicts

--- src/routes/route_check/results_export/+page.svelte
+++ src/routes/route_check/results_export/+page.svelte
@@ -6,8 +6,22 @@
   import Overview from "../results_summary/Overview.svelte";
   import PolicyCheckResults from "../results_analysis/PolicyCheckResults.svelte";
   import SafetyCheckResults from "../results_analysis/SafetyCheckResults.svelte";
+  import {
+    criticalIssueChoices,
+    policyConflictChoices,
+  } from "../problems_map/lists";
 
   let results = getResults($state);
+
+  // Hack around
+  // https://stackoverflow.com/questions/67336062/typescript-not-parsed-in-svelte-html-section
+  // until we're using Svelte 5
+  function notNull<T>(x: T | null | undefined): T {
+    if (x == null || x == undefined) {
+      throw new Error("Oops, notNull given something null");
+    }
+    return x;
+  }
 </script>
 
 <SummaryOfScheme />
@@ -19,6 +33,54 @@
 
 <PolicyCheckResults />
 
+<table>
+  <tr>
+    <th>Remaining Policy Conflicts</th>
+    <th>Policy Conflict ID</th>
+    <th>Policy Conflict</th>
+    <th>Location Name</th>
+    <th>Commentary & Feedback</th>
+  </tr>
+  {#each $state.policyConflictLog.filter((x) => x.resolved != "Yes") as conflict, i}
+    <tr>
+      <th>{i + 1}</th>
+      <td>TODO ID</td>
+      <td>
+        {notNull(
+          policyConflictChoices.find((pair) => pair[0] == conflict.conflict),
+        )[1]}
+      </td>
+      <td>{conflict.locationName}</td>
+      <td>{conflict.notes}</td>
+    </tr>
+  {/each}
+</table>
+
 <SafetyCheckResults />
+
+<table>
+  <tr>
+    <th>Remaining Critical Issues</th>
+    <th>Critical ID</th>
+    <th>Critical Issue</th>
+    <th>Location Name</th>
+    <th>Commentary & Feedback</th>
+  </tr>
+  {#each $state.criticalIssues.filter((x) => x.resolved != "Yes") as critical, i}
+    <tr>
+      <th>{i + 1}</th>
+      <td>TODO ID</td>
+      <td>
+        {notNull(
+          criticalIssueChoices.find(
+            (pair) => pair[0] == critical.criticalIssue,
+          ),
+        )[1]}
+      </td>
+      <td>{critical.locationName}</td>
+      <td>{critical.notes}</td>
+    </tr>
+  {/each}
+</table>
 
 <CheckDetails {results} />
