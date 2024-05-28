4d6a0f36ea69c29981315223b00b0d148346b424
Dustin Carlino <dabreegster@gmail.com>
2024-05-28 14:03:22 +0100

    Format the planning radio better

---
+++ src/routes/planning/scorecard/ColoredRadio.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  import { v4 as uuidv4 } from "uuid";
+
+  // A list of [value, label, background color, font color] representing the choices
+  export let choices: [string, string, string, string][];
+  export let value: string;
+
+  let id = uuidv4();
+</script>
+
+<div class="govuk-grid-row">
+  <div class="govuk-grid-column-one-quarter">
+    <!-- svelte-ignore a11y-label-has-associated-control TODO Splitting the radio -->
+    <label class="govuk-label govuk-label--s">Rating</label>
+  </div>
+  <div class="govuk-grid-column-three-quarters">
+    <!-- Empty -->
+  </div>
+</div>
+
+{#each choices as [thisValue, thisLabel, thisBackgroundColor, thisFontColour], i}
+  <div class="govuk-grid-row">
+    <div class="govuk-grid-column-one-quarter">
+      <div class="govuk-radios__item">
+        <input
+          class="govuk-radios__input"
+          id={id + thisValue}
+          type="radio"
+          value={thisValue}
+          bind:group={value}
+        />
+        <label class="govuk-label govuk-radios__label" for={id + thisValue}>
+          <span
+            class="govuk-tag"
+            style:background={thisBackgroundColor}
+            style:color={thisFontColour}
+          >
+            {thisValue}
+          </span>
+        </label>
+      </div>
+    </div>
+
+    <div
+      class="govuk-grid-column-three-quarters"
+      style="border-left: 5px solid {thisBackgroundColor}"
+    >
+      <p>{thisLabel}</p>
+    </div>
+  </div>
+{/each}
--- src/routes/planning/scorecard/Question.svelte
+++ src/routes/planning/scorecard/Question.svelte
@@ -1,56 +1,80 @@
 <script lang="ts">
   import { PrevNext } from "$lib";
-  import { CollapsibleCard, Radio, TextArea } from "govuk-svelte";
+  import { CollapsibleCard, TextArea } from "govuk-svelte";
+  import ColoredRadio from "./ColoredRadio.svelte";
   import Progress from "./Progress.svelte";
   import { state } from "../data";
 
   export let idx: number;
   export let label: string;
 
-  let choices: [string, string][] = [
+  let choices: [string, string, string, string][] = [
     [
       "Exemplar",
       "The proposal exceeds the standards in policy and guidance and represents an example of best practice.",
+      "#00b050",
+      "white",
     ],
     [
       "Pass",
       "The proposal meets standards in policy and guidance. Where there are shortfalls, these are minor and do not affect the overall position.",
+      "#99cc00",
+      "black",
     ],
     [
       "Condition / Obligation to make acceptable",
       "The proposal does not adhere to standards in policy and guidance; however a planning condition/obligation can address any shortfalls.",
+      "#ffc000",
+      "black",
     ],
     [
       "Concern",
       "The proposal does not adhere to standards in policy and guidance and more detail is needed to inform the assessment of the proposal. This may include reasonable amendments, improvements to infrastructure and/or additional information.",
+      "#ff5050",
+      "black",
     ],
     [
       "Critical issue",
       "The proposal represents a significant departure from standards in policy and guidance. Either comprehensive amendments or the delivery of new infrastructure would be required; or it is unlikely that the deficiency can be addressed.",
+      "#ff0000",
+      "white",
     ],
-    ["Not applicable", "This criterion is not a material consideration."],
+    [
+      "Not applicable",
+      "This criterion is not a material consideration.",
+      "grey",
+      "white",
+    ],
   ];
 </script>
 
 <PrevNext {idx} total={10} urlPath="planning/scorecard/q" />
 
-<h1>{idx}. {label}</h1>
+<div class="govuk-grid-row" style="margin: 1em">
+  <div class="govuk-grid-column-one-quarter">
+    <Progress currentIdx={idx} />
+  </div>
 
-<slot name="description" />
+  <div class="govuk-grid-column-three-quarters">
+    <h2>{idx}. {label}</h2>
 
-<Radio legend="Rating" {choices} bind:value={$state.ratings[idx - 1]} />
+    <slot name="description" />
 
-<TextArea
-  label="Appraiser Comments"
-  bind:value={$state.appraiserComments[idx - 1]}
-/>
+    <ColoredRadio {choices} bind:value={$state.ratings[idx - 1]} />
 
-<CollapsibleCard label="Common Shortfalls" open>
-  <slot name="shortfalls" />
-</CollapsibleCard>
+    <TextArea
+      label="Appraiser Comments"
+      bind:value={$state.appraiserComments[idx - 1]}
+    />
 
-<CollapsibleCard label="Policy & Guidance" open>
-  <slot name="guidance" />
-</CollapsibleCard>
+    <CollapsibleCard label="Common Shortfalls" open>
+      <slot name="shortfalls" />
+    </CollapsibleCard>
+
+    <CollapsibleCard label="Policy & Guidance" open>
+      <slot name="guidance" />
+    </CollapsibleCard>
+  </div>
+</div>
 
 <PrevNext {idx} total={10} urlPath="planning/scorecard/q" />
