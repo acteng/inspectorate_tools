b2bcd368dcd7201f0ac4f67f2e4cf686a04cb6d1
Dustin Carlino <dabreegster@gmail.com>
2024-06-19 11:34:48 +0100

    Format policy check page better

--- src/routes/route_check/policy_check/+page.svelte
+++ src/routes/route_check/policy_check/+page.svelte
@@ -13,14 +13,16 @@
   ];
 </script>
 
-{#each questions as question, idx}
-  <h2>P00{idx + 1}: {question}</h2>
-  <div style="display: flex; justify-content: space-evenly">
-    <YesNo label="Existing" bind:value={$state.policyCheck[idx].existing} />
-    <YesNo label="Proposed" bind:value={$state.policyCheck[idx].proposed} />
-  </div>
-  <TextArea
-    label="Commentary"
-    bind:value={$state.policyCheck[idx].commentary}
-  />
-{/each}
+<div class="govuk-width-container">
+  {#each questions as question, idx}
+    <h2>P00{idx + 1}: {question}</h2>
+    <div style="display: flex; justify-content: space-evenly">
+      <YesNo label="Existing" bind:value={$state.policyCheck[idx].existing} />
+      <YesNo label="Proposed" bind:value={$state.policyCheck[idx].proposed} />
+    </div>
+    <TextArea
+      label="Commentary"
+      bind:value={$state.policyCheck[idx].commentary}
+    />
+  {/each}
+</div>
