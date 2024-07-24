60e4afc67af02d2749849298ca0c5512610a92c9
Dustin Carlino <dabreegster@gmail.com>
2024-07-24 14:17:06 +0100

    Detect when the user is editing the same file in multiple tabs and warn about it

--- src/routes/route_check/+layout.svelte
+++ src/routes/route_check/+layout.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
-  import { state, currentFile } from "./data";
+  import { state, currentFile, files } from "./data";
   import { base } from "$app/paths";
   import { NavHeader, NavFooter, getTitle } from "$lib/nav";
   import { page } from "$app/stores";
   import folderUrl from "$lib/assets/images/folder.svg?url";
+
+  function storageChange(ev: StorageEvent) {
+    if (files.key($currentFile) == ev.key) {
+      window.alert(
+        "The file you're editing just changed in another browser tab. Please don't edit the same file in multiple tabs at the same time; your changes in one of the tabs will be lost.",
+      );
+    }
+  }
 </script>
+
+<svelte:window on:storage={storageChange} />
 
 <div>
   <img src={folderUrl} alt="Manage files" style="vertical-align: middle;" />
