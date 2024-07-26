a2af5fdf3ba7c256bf0e781822a580876b59f01b
Dustin Carlino <dabreegster@gmail.com>
2024-07-26 17:24:18 +0100

    Use images for the panel controls (but need to adjust styling)

---
+++ src/lib/IconButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { SecondaryButton } from "govuk-svelte";
+
+  // Removes the margin beneath this button, so it can be used as a floating map control
+
+  export let disabled = false;
+</script>
+
+<SecondaryButton on:click style="margin-bottom: 0px" {disabled}>
+  <slot />
+</SecondaryButton>
