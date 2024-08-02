1c6242cb3aa8c51d3d994453a9734fba67355387
Dustin Carlino <dabreegster@gmail.com>
2024-08-02 11:43:30 +0100

    Restyle the header

--- src/lib/assets/images/README.md
+++ src/lib/assets/images/README.md
@@ -1,1 +1,3 @@
-Except for `right-arrow.svg`, icons are from [Material Icons](https://fonts.google.com/icons)
+Except for `right-arrow.svg`, icons are from [Material Icons](https://fonts.google.com/icons).
+
+Crest is from https://www.gov.uk/government/organisations/active-travel-england
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,10 @@
   import "../style/main.css";
   // @ts-expect-error no declarations
   import { initAll } from "govuk-frontend";
-  import { WarningText } from "govuk-svelte";
+  import { AlphaBanner } from "govuk-svelte";
   import { page } from "$app/stores";
   import { getTitle } from "$lib/nav";
+  import logoUrl from "$lib/assets/images/ate_logo.png?url";
 
   initAll();
 </script>
@@ -13,22 +14,25 @@
   <title>{getTitle($page.url.pathname)}</title>
 </svelte:head>
 
-<WarningText>
-  This is an experimental tool by <a href="mailto:dcarlino@turing.ac.uk">
-    Dustin
-  </a>
-  and
-  <a href="mailto:Peter.York@activetravelengland.gov.uk">Pete</a>
-  . Use only for internal testing. Please give feedback in the
-  <a
-    href="https://departmentfortransportuk.sharepoint.com/:x:/r/sites/ATE-Data-and-DigitalRLG-ATE-ATE/Shared%20Documents/ATIP/ATE%20Tools%202024%20Feedback%20Log_V2.xlsx?d=w31a14d35e31f46a8b9cf6f4f697b2692&csf=1&web=1&e=jLUWFN"
-    target="_blank"
-  >
-    Web Tools tab
-  </a>
-  .
-</WarningText>
-
 <div class="govuk-prose">
+  <div class="govuk-width-container">
+    <img src={logoUrl} alt="Active Travel England logo" height="30" />
+  </div>
+
+  <div class="green-bar" />
+
+  <div class="govuk-width-container" style="margin-bottom: 30px">
+    <AlphaBanner />
+  </div>
+
+  <!-- It's up to each page to use govuk-width-container or not -->
   <slot />
 </div>
+
+<style>
+  .green-bar {
+    width: 100vw;
+    margin-left: -8px;
+    border-bottom: 0.5rem solid #007161;
+  }
+</style>
