414331a9e49692dd1b9f665f59e105a6fa69872a
Peter York <peter.york@dft.gov.uk>
2024-08-06 11:43:43 +0100

    context guidance for JAT tool

--- src/routes/route_check/jat_check/guidance/MovementAssessment.svelte
+++ src/routes/route_check/jat_check/guidance/MovementAssessment.svelte
@@ -1,63 +1,76 @@
 <script lang="ts">
+  import { Modal } from "$lib";
+  import { SecondaryButton } from "govuk-svelte";
   export let junctionType: string;
   export let cycleMovement: string;
+  export let summary: string = "";
   export let scoreZeroBullets: string[];
   export let scoreOneBullets: string[];
   export let scoreTwoBullets: string[];
+
+  let showModal = false;
+
+  function openModal() {
+    showModal = true;
+  }
 </script>
 
-<h3>{junctionType}: {cycleMovement}</h3>
-<table>
-  <tr>
-    <th class="has-hover score-zero-header">
-      Score = 0
-      <span class="hover-text">
-        Suitable only for confident existing cyclists, and may be avoided by
-        some experienced cyclists Conditions are most likely to give rise to the
-        most common collision types
-      </span>
-    </th>
-    <th class="has-hover score-one-header">
-      Score = 1
-      <span class="hover-text">
-        Likely to be more acceptable to most cyclists, but may still pose
-        problems for less confident or new cyclists The risk of collisions has
-        been reduced by design layout or traffic management interventions
-      </span>
-    </th>
-    <th class="has-hover score-two-header">
-      Score = 2
-      <span class="hover-text">
-        Suitable for all potential and existing cyclists The potential for
-        collisions has been removed, or managed to a high standard of safety for
-        cyclists
-      </span>
-    </th>
-  </tr>
-  <tr>
-    <td class="score-zero">
-      <ul>
-        {#each scoreZeroBullets as bulletPoint}
-          <li>{bulletPoint}</li>
-        {/each}
-      </ul>
-    </td>
-    <td class="score-one">
-      <ul>
-        {#each scoreOneBullets as bulletPoint}
-          <li>{bulletPoint}</li>
-        {/each}
-      </ul>
-    </td>
-    <td class="score-two">
-      <ul>
-        {#each scoreTwoBullets as bulletPoint}
-          <li>{bulletPoint}</li>
-        {/each}
-      </ul>
-    </td>
-  </tr>
-</table>
+<div>
+  <SecondaryButton on:click={openModal}>Display guidance</SecondaryButton>
+</div>
+<Modal title={`Guidance`} bind:open={showModal}>
+  <h3>{`${junctionType}: ${cycleMovement}`}</h3>
+  {#if summary}
+    <p>{summary}</p>
+  {/if}
+  <table>
+    <tr>
+      <th class="score-zero-header">
+        Score = 0
+        <br />
+        Suitable only for confident existing cyclists, and may be avoided by some
+        experienced cyclists Conditions are most likely to give rise to the most
+        common collision types
+      </th>
+      <th class="score-one-header">
+        Score = 1
+        <br />
+        Likely to be more acceptable to most cyclists, but may still pose problems
+        for less confident or new cyclists The risk of collisions has been reduced
+        by design layout or traffic management interventions
+      </th>
+      <th class="has-hover score-two-header">
+        Score = 2
+        <br />
+        Suitable for all potential and existing cyclists The potential for collisions
+        has been removed, or managed to a high standard of safety for cyclists
+      </th>
+    </tr>
+    <tr>
+      <td class="score-zero">
+        <ul>
+          {#each scoreZeroBullets as bulletPoint}
+            <li>{bulletPoint}</li>
+          {/each}
+        </ul>
+      </td>
+      <td class="score-one">
+        <ul>
+          {#each scoreOneBullets as bulletPoint}
+            <li>{bulletPoint}</li>
+          {/each}
+        </ul>
+      </td>
+      <td class="score-two">
+        <ul>
+          {#each scoreTwoBullets as bulletPoint}
+            <li>{bulletPoint}</li>
+          {/each}
+        </ul>
+      </td>
+    </tr>
+  </table>
+</Modal>
 
 <style>
   .score-zero-header {
@@ -83,19 +96,8 @@
     background-color: #e5edd8;
   }
 
-  td, th {
+  td,
+  th {
     padding: 0.25em;
-  }
-
-  .has-hover {
-    position: relative;
-  }
-  .hover-text {
-    visibility: hidden;
-    position: absolute;
-    z-index: 1;
-  }
-  .has-hover:hover .hover-text {
-    visibility: visible;
   }
 </style>
