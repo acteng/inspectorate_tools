033a7e3829a33df8443817dfca8c352c749af6e3
Peter York <peter.york@dft.gov.uk>
2024-08-06 11:43:43 +0100

    context guidance for JAT tool

--- src/routes/route_check/jat_check/guidance/Guidance.svelte
+++ src/routes/route_check/jat_check/guidance/Guidance.svelte
@@ -1,28 +1,47 @@
 <script lang="ts">
+  import { Radio } from "govuk-svelte";
+  import { pairs } from "$lib";
   import MovementAssesment from "./MovementAssessment.svelte";
+  import { guidance } from "./data.ts";
+
+  let junctionTypes: string[] = Object.keys(guidance);
+  let selectedJunctionType: string = junctionTypes[0];
+  let movementTypes: string[] = [];
+  let selectedMovementType: string = "";
+
+  $: {
+    movementTypes = Object.keys(guidance[selectedJunctionType]).filter(
+      (key) => key !== "summary",
+    );
+    if (movementTypes.length === 1) {
+      selectedMovementType = movementTypes[0];
+    }
+  }
 </script>
 
-<h3>Generic Guidance</h3>
-<MovementAssesment
-  junctionType={"Any type of junction"}
-  cycleMovement={"Any movement"}
-  scoreZeroBullets={[
-    "Cycle movement in potential conflict with heavy motor traffic flow",
-    "Cycle movement mixed with or crossing traffic with 85th percentile speed exceeding 60kph, or where vehicles accelerate rapidly.",
-    "Necessary to cross more than one traffic lane (without refuge or protection) to complete cycle movement unless traffic flows are low.",
-    "Cycle movementcrosses wide junctionentry or exit: e.g. withmerge or diverge taper or slip lane.",
-    "Pinch points on junction entry or exit (lane width 3.2m-3.9m).",
-    "Cycle movement affected by very poor surface quality utility reinstatement, gully positioning, debris.",
-  ]}
-  scoreOneBullets={[
-    "Cycle movement in potential conflict with moderate traffic flow.",
-    "Cycle lanes through junction meeting appropriate desirable minimum width requirements for the movement under consideration.",
-    "Raised table at junction crossed by traffic in potential conflict with cycle movement.",
-    "Cycle movement made by transiting onto section of shared use footway.",
-  ]}
-  scoreTwoBullets={[
-    "Low traffic speed and volume in mixed traffic environment (e.g. accessonly streets in a residential area).",
-    "Cycle movement separated physically and/or in time from motor traffic and also separated from pedestrians.",
-    "Cycle movement bypasses junction completely, including via good quality grade separation",
-  ]}
+<Radio
+  label="What type of junction do you need guidance for"
+  choices={pairs(junctionTypes)}
+  bind:value={selectedJunctionType}
 />
+{#if movementTypes.length > 1}
+  <Radio
+    label="What type of movement do you need guidance for"
+    choices={pairs(movementTypes)}
+    bind:value={selectedMovementType}
+  />
+{/if}
+
+{#if guidance[selectedJunctionType][selectedMovementType]}
+  <MovementAssesment
+    junctionType={selectedJunctionType}
+    cycleMovement={selectedMovementType}
+    summary={guidance[selectedJunctionType].summary}
+    scoreZeroBullets={guidance[selectedJunctionType][selectedMovementType]
+      .scoreZero}
+    scoreOneBullets={guidance[selectedJunctionType][selectedMovementType]
+      .scoreOne}
+    scoreTwoBullets={guidance[selectedJunctionType][selectedMovementType]
+      .scoreTwo}
+  />
+{/if}
