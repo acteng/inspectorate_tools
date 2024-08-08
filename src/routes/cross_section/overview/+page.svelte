5e3be93b402988eaf645f10952d6af016a89e8f7
Dustin Carlino <dabreegster@gmail.com>
2024-08-08 11:20:00 +0100

    Rearrange file manager again

--- src/routes/cross_section/overview/+page.svelte
+++ src/routes/cross_section/overview/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { base } from "$app/paths";
   import { ExternalLink } from "$lib";
+  import { currentFile, state } from "../data";
+  import { ExportFile } from "$lib/files";
 </script>
 
 <div class="govuk-width-container">
@@ -11,6 +13,10 @@
       design assistance tool
     </ExternalLink>.
   </p>
+
+  <ExportFile {currentFile} {state} />
+
+  <p>Check what you need to do to assess the design quality of a scheme.</p>
 
   <ol>
     <li><a href="{base}/cross_section/summary">Summary of Scheme</a></li>
--- src/routes/planning/overview/+page.svelte
+++ src/routes/planning/overview/+page.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
   import { base } from "$app/paths";
+  import { currentFile, state } from "../data";
+  import { ExportFile } from "$lib/files";
 </script>
 
 <div class="govuk-width-container">
   <p>This is an experimental version of an internal tool.</p>
+
+  <ExportFile {currentFile} {state} />
+
+  <p>
+    Check what you need to do to assess the design quality of an application.
+  </p>
 
   <ol>
     <li><a href="{base}/planning/app_details">Application details</a></li>
