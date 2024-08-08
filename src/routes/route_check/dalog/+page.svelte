bdcfde7759c0b4ef6ccb9de1b8d823d685cbd5ce
Dustin Carlino <dabreegster@gmail.com>
2024-08-08 16:49:22 +0100

    Simplify dalog page

--- src/routes/route_check/dalog/+page.svelte
+++ src/routes/route_check/dalog/+page.svelte
@@ -4,10 +4,8 @@
   import { TextArea } from "govuk-svelte";
   import ConvertToXlsx from "../results_export/ConvertToXlsx.svelte";
 
-  let pairs = encodeDalog($state);
-  let header = pairs.map((pair) => pair[0]).join("\t");
   // TODO Need to escape " in the values, then
-  let values = pairs.map((pair) => `"${pair[1]}"`).join("\t");
+  $: values = encodeDalog($state).map((pair) => `"${pair[1]}"`).join("\t");
 </script>
 
 <div class="govuk-width-container">
@@ -18,12 +16,5 @@
     values), copyable into Excel.
   </p>
 
-  <TextArea label="TSV output, just the one row of values" value={values} />
-
-  <TextArea label="TSV output with header" value={`${header}\n${values}\n`} />
-
-  <TextArea
-    label="JSON mapping"
-    value={JSON.stringify(Object.fromEntries(pairs), null, "  ")}
-  />
+  <TextArea label="TSV output" value={values} />
 </div>
