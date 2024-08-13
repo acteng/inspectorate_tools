620b8541dfe9ac258587478bfd54b46b8e519c82
Dustin Carlino <dabreegster@gmail.com>
2024-08-13 14:35:44 +0100

    Show task status on overview page, without a way to detect completion yet

--- src/routes/route_check/overview/+page.svelte
+++ src/routes/route_check/overview/+page.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
   import { base } from "$app/paths";
-  import { currentFile, state } from "../data";
+  import { currentFile, state, emptyState, type State } from "../data";
   import { ExportFile } from "$lib/files";
   import ConvertToXlsx from "../results_export/ConvertToXlsx.svelte";
+  import Task from "./Task.svelte";
+
+  let empty = emptyState();
+
+  type Status = "Not yet started" | "In progress" | "Completed";
+
+  function statusTag(key: keyof State): Status {
+    return cmp((x) => x[key]);
+  }
+
+  function cmp(f: (x: State) => any): Status {
+    return JSON.stringify(f($state)) == JSON.stringify(f(empty))
+      ? "Not yet started"
+      : "In progress";
+  }
+
+  function pmp(x: State): Status {
+    let len = x.policyConflictLog.length + x.criticalIssues.length;
+    if (len == 0) {
+      return "Not yet started";
+    }
+    return "In progress";
+  }
 </script>
 
 <div class="govuk-width-container">
@@ -13,44 +36,77 @@
   <p>Check what you need to do to assess the design quality of a scheme.</p>
 
   <ol>
+    <hr />
     <li><a href="{base}/route_check/overview">Overview</a></li>
-    <li><a href="{base}/route_check/summary">Summary of Scheme</a></li>
-    <li><a href="{base}/route_check/route_map">Route Map</a></li>
-    <li><a href="{base}/route_check/policy_check">Policy Check</a></li>
-
-    <li><a href="{base}/route_check/safety_check">Safety Check</a></li>
-    <li>
-      <a href="{base}/route_check/problems_map">Problems Mapping Page</a>
-    </li>
+    <hr />
+    <Task
+      link="summary"
+      title="Summary of Scheme"
+      taskStatus={statusTag("summary")}
+    />
+    <Task
+      link="route_map"
+      title="Route Map"
+      taskStatus={cmp((x) => x.summary.networkMap)}
+    />
+    <Task
+      link="policy_check"
+      title="Policy Check"
+      taskStatus={statusTag("policyCheck")}
+    />
+    <Task
+      link="safety_check"
+      title="Safety Check"
+      taskStatus={statusTag("safetyCheck")}
+    />
+    <Task
+      link="problems_map"
+      title="Problem Mapping Page"
+      taskStatus={pmp($state)}
+    />
 
     {#if $state.summary.checkType == "street"}
-      <li><a href="{base}/route_check/street_check">Street Check</a></li>
-      <li>
-        <a href="{base}/route_check/street_placemaking_check">
-          Street Placemaking Check
-        </a>
-      </li>
+      <Task
+        link="street_check"
+        title="Street Check"
+        taskStatus={statusTag("streetCheck")}
+      />
+      <Task
+        link="street_placemaking_check"
+        title="Street Placemaking Check"
+        taskStatus={statusTag("streetPlacemakingCheck")}
+      />
     {:else if $state.summary.checkType == "path"}
-      <li><a href="{base}/route_check/path_check">Path Check</a></li>
-      <li>
-        <a href="{base}/route_check/path_placemaking_check">
-          Path Placemaking Check
-        </a>
-      </li>
+      <Task
+        link="path_check"
+        title="Path Check"
+        taskStatus={statusTag("pathCheck")}
+      />
+      <Task
+        link="path_placemaking_check"
+        title="Path Placemaking Check"
+        taskStatus={statusTag("pathPlacemakingCheck")}
+      />
     {:else}
       <li>Street/Path Check</li>
+      <hr />
       <li>Street/Path Placemaking Check</li>
+      <hr />
     {/if}
 
-    <li><a href="{base}/route_check/jat_check">JAT Check</a></li>
+    <Task link="jat_check" title="JAT Check" taskStatus={statusTag("jat")} />
 
     <li>
       <a href="{base}/route_check/results_summary">Results Summary</a>
     </li>
+    <hr />
     <li>
       <a href="{base}/route_check/results_analysis">Results Further Analysis</a>
     </li>
+    <hr />
     <li><a href="{base}/route_check/results_export">Results Export</a></li>
+    <hr />
     <li><a href="{base}/route_check/dalog">Design Assistance Log</a></li>
+    <hr />
   </ol>
 </div>
---
+++ src/routes/route_check/overview/Task.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { base } from "$app/paths";
+
+  export let link: string;
+  export let title: string;
+  export let taskStatus: "Not yet started" | "In progress" | "Completed";
+</script>
+
+<li>
+  <a href="{base}/route_check/{link}">{title}</a>
+  {#if taskStatus == "Not yet started"}
+    <strong class="govuk-tag govuk-tag--blue" style="float: right">
+      {taskStatus}
+    </strong>
+  {:else if taskStatus == "In progress"}
+    <strong class="govuk-tag govuk-tag--light-blue" style="float: right">
+      {taskStatus}
+    </strong>
+  {:else if taskStatus == "Completed"}
+    <span style="float: right">{taskStatus}</span>
+  {/if}
+</li>
+<hr />
