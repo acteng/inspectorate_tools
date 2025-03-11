1a5906684ec8c745dc7c03a4b8de8e5014e60bcb
Peter York <peter.york@dft.gov.uk>
2025-03-11 17:09:51 +0000

    Sort authority names

--- src/lib/lists.ts
+++ src/lib/lists.ts
@@ -37,7 +37,7 @@ const additionalAuthorities = [
   "Westminster City Council",
 ];
 
-export let authorities = ["N/A", ...authorityNames, ...additionalAuthorities];
+export let authorities = ["N/A", ...[...authorityNames, ...additionalAuthorities].sort()];
 
 export let transportOrCombinedAuthorities = [
   "Not applicable",
