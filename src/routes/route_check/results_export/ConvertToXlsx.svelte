a1a656883271259ad6ccece30cb73a11068107ac
Pete-Y-CS <133757676+Pete-Y-CS@users.noreply.github.com>
2025-04-02 12:26:47 +0100

    Various fixes - resolves  #112 (#114)
    * Add 'please specify' to 'other' options on summary pages
    
    * Add decimal places reminder
    
    * Update the xlsx export caveats
    
    ---------
    
    Co-authored-by: Peter York <peter.york@dft.gov.uk>

--- src/routes/route_check/results_export/ConvertToXlsx.svelte
+++ src/routes/route_check/results_export/ConvertToXlsx.svelte
@@ -39,6 +39,19 @@
         everything mapped when the route is split into multiple pieces.
       </li>
       <li>The map weblink only shows the route start point.</li>
+      <li>
+        Graphs will not be exported. To share a summary of results, export
+        relevant results pages as .pdf files from your we browser.
+      </li>
+      <li>
+        Formatting changes may occur, including spreadsheet tab colours and
+        other minor inconsistencies.
+      </li>
+      <li>
+        In the JAT tab, only the scores will be exported - movement diagrams
+        will be lost.
+      </li>
+      <li>The spreadsheet will be completely unprotected.</li>
     </ul>
   </WarningText>
   <SecondaryButton on:click={() => (open = false)}>OK</SecondaryButton>
