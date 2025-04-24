7109c3b0a42169d2384087b5c1b0a69aadcf8e43
Peter York <peter.york@dft.gov.uk>
2025-04-24 14:15:26 +0100

    Change from hint to warning

--- src/lib/DateInput.svelte
+++ src/lib/DateInput.svelte
@@ -4,14 +4,20 @@
 
   export let label: string;
   export let value: string;
-  export let showImportHint: boolean = false;
+  export let showImportWarning: boolean = false;
 
   let id = uuidv4();
 </script>
 
 <FormElement {label} {id}>
-  {#if showImportHint}
-    <div class="govuk-hint">NB. This doesn't import from the excel version</div>
+  {#if showImportWarning}
+    <div class="govuk-warning-text">
+      <span class="govuk-warning-text__icon" aria-hidden="true">!</span>
+      <strong class="govuk-warning-text__text">
+        <span class="govuk-visually-hidden">Warning</span>
+        NB. This date isn't imported from the excel version
+      </strong>
+    </div>
   {/if}
   <input type="date" bind:value />
 </FormElement>
