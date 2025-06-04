b6c7bb2ae3b65227481d838934cdbcf679c17615
Peter York <peter.york@dft.gov.uk>
2025-06-04 11:26:19 +0100

    Allow user to upload PYATS sketch as route map

--- src/routes/route_check/RouteMapLayer.svelte
+++ src/routes/route_check/RouteMapLayer.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import { GeoJSON, LineLayer } from "svelte-maplibre";
+  import { isLine, isPoint, isPolygon } from "scheme-sketcher-lib/maplibre";
+  import { CircleLayer, FillLayer, GeoJSON, LineLayer } from "svelte-maplibre";
   import { state } from "./data";
 
   export let show = true;
@@ -7,9 +8,28 @@
 </script>
 
 <GeoJSON data={$state.summary.networkMap}>
+  <CircleLayer
+    id={`${id}-points`}
+    paint={{ "circle-color": "blue", "circle-radius": 10 }}
+    filter={["all", isPoint]}
+    layout={{ visibility: show ? "visible" : "none" }}
+  />
+  <FillLayer
+    id={`${id}-polygon-fill`}
+    paint={{ "fill-color": "red", "fill-opacity": 0.2 }}
+    filter={["all", isPolygon]}
+    layout={{ visibility: show ? "visible" : "none" }}
+  />
   <LineLayer
-    {id}
+    id={`${id}-polygon-outlines`}
+    paint={{ "line-color": "black", "line-width": 2 }}
+    filter={["all", isPolygon]}
+    layout={{ visibility: show ? "visible" : "none" }}
+  />
+  <LineLayer
+    id={`${id}-lines`}
     paint={{ "line-color": "cyan", "line-width": 5 }}
+    filter={["all", isLine]}
     layout={{ visibility: show ? "visible" : "none" }}
   />
 </GeoJSON>
