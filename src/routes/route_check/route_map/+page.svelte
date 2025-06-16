a4d051aef52e7eda8f1a214183f2184419da0f80
Peter York <peter.york@dft.gov.uk>
2025-06-16 14:01:03 +0100

    Improve the route map - on file load auto-zoom - add link to start sketching

--- src/routes/route_check/route_map/+page.svelte
+++ src/routes/route_check/route_map/+page.svelte
@@ -28,6 +28,7 @@
   import { state } from "../data";
   import RouteMapLayer from "../RouteMapLayer.svelte";
   import { getBestMatch, loadAuthorities } from "./match_area";
+    import ExternalLink from "$lib/ExternalLink.svelte";
 
   onMount(async () => {
     await loadAuthorities();
@@ -123,6 +124,7 @@
       let value = JSON.parse(contents);
       $state.summary.networkMap = value;
       $gjSchemes = value;
+      initiallyZoom($mapStore)
     } catch (err) {
       window.alert(`Couldn't load ${rawFilename}: ${err}`);
     }
@@ -163,6 +165,7 @@
             label={`Import existing Plan Your Active Travel Schemes Sketch`}
             onLoad={importSketchFile}
           />
+          <ExternalLink href="https://plan.activetravelengland.gov.uk/choose_area.html">Sketch using PYATS</ExternalLink>
         </div>
 
         {#if routeAuthority}
